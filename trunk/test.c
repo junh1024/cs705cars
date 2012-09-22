4fcc305f5d72510d622f07242ca59f48bc0d950d
junh1024@gmail.com <junh1024@gmail.com@36892dda-2c90-5672-c6f2-4098cb2dc41a>
2012-09-22 02:25:27 +0000

    test change

--- trunk/test.c
+++ trunk/test.c
@@ -4,17 +4,17 @@ FILE * iFile, * oFile;
 
 int main (int argc, char* argv[])
 {
-	// iFile = fopen (argv[1],"r");
-	// oFile = fopen (argv[2],"w+");
+	iFile = fopen (argv[1],"r");
+	oFile = fopen (argv[2],"w+");
 	
-	// while line != NULL:
-		// fscanf (iFile,"%s",str1);
-		// fscanf(fp,"%d",&numberOfPeople); 
+	while line != NULL:
+		fscanf (iFile,"%s",str1);
+		fscanf(fp,"%d",&numberOfPeople); 
 		
-	// fprintf (oFile,"%f %f %d\n",__CruiseControl_V13,__CruiseControl_V14,__CruiseControl_V15);
+	fprintf (oFile,"%f %f %d\n",__CruiseControl_V13,__CruiseControl_V14,__CruiseControl_V15);
 	
-		// fclose(iFile);
-	// fclose(oFile);
-	// return 0;
-	printf("hello world");
+		fclose(iFile);
+	fclose(oFile);
+	return 0;
+	// printf("hello world");
 }
