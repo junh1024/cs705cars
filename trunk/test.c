2aa720b45a2f35c32463ad707c82e4ee0fa7491c
junh1024@gmail.com <junh1024@gmail.com@36892dda-2c90-5672-c6f2-4098cb2dc41a>
2012-09-27 22:44:20 +0000

    modified test to not use previous positions, results changed

--- trunk/test.c
+++ trunk/test.c
@@ -3,15 +3,10 @@
 #include <math.h>
 
 float car1pos=-5;
-float car1prevpos=-5;
-
 float car2pos=-20;
-float car2prevpos=-20;
 
 int car1speed=0;
 int car2speed=0;
-int car1prevspeed=0;
-int car2prevspeed=0;
 
 float anumber;
 
@@ -29,48 +24,27 @@ int main (int argc, char* argv[])
 	while ( fscanf(iFile,"%f",&anumber) != EOF )//car1pos
 	{
 		car1pos=anumber;
-		if(fabs(car2pos-car1pos )< 5)//smaller than 5 meters abs distace to car1
-		{	
-			
-			car2speed=car2prevspeed-10; //slow down
-			if(car2speed<0) //can't have negative speed
-				{car2speed=0;}
+		if(fabs(car2pos-car1pos )< 7)//smaller than 7 meters abs distace to car1
+		{				
+			car2speed=-15; //slow down
 		}
 		else
 		{
-			if (car2prevspeed>=60)//over speed limit, 16cm per 0.1s=1.6m for 1s assume speed limit for road is 60kmh =~1.67m/0.1s
-			{	car2speed=60;}
-			else
-			{	car2speed=car2prevspeed+5;}//speed up
+			car2speed+=5;//speed up
 		}
-		car2pos=car2prevpos+(car2speed/3.6);
-		car2prevpos=car2pos;
-		car2prevspeed=car2speed;
-		
-		fprintf(oFile,"%f\n",car2pos);
-		// fprintf(oFile,"%f\n",car2prevpos);
-		// fprintf(oFile,"%f\n",car2speed);
-		// fprintf(oFile,"%f\n",car2prevspeed);
-		
-		
-	}
-	
+			
+		if(car2speed<0) //can't have negative speed
+		{
+			car2speed=0;
+		}
+		if (car2speed>=60)//over speed limit of 60kmh
+		{
+			car2speed=60;
+		}
+		car2pos+=(car2speed/3.6);
 
-	
-		// for(i=0;i<lagread;i++)
-		// {
-			// printf("%f\r\n",car1posbuf[i]);
-		// }
-	
-		// for(i=0;i<cardatalen;i++)
-		// {
-			// fprintf(oFile,"%f\n",car2pos[i]);
-		// }
-		
-		// for(i=0;i<cardatalen;i++)
-		// {
-			// fprintf(oFile,"%f\n",car2speed[i]);
-		// }
+		fprintf(oFile,"%f\n",car2pos);
+	}
 	
 	fclose(iFile);
 	fclose(oFile);
