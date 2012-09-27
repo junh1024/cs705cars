20f1ee16c9b0b4d0b467b387b101827efad14526
junh1024@gmail.com <junh1024@gmail.com@36892dda-2c90-5672-c6f2-4098cb2dc41a>
2012-09-27 11:00:19 +0000

    change units to khm for speed

--- trunk/test.c
+++ trunk/test.c
@@ -8,10 +8,10 @@ float car1prevpos=-5;
 float car2pos=-20;
 float car2prevpos=-20;
 
-float car1speed=0;
-float car2speed=0;
-float car1prevspeed=0;
-float car2prevspeed=0;
+int car1speed=0;
+int car2speed=0;
+int car1prevspeed=0;
+int car2prevspeed=0;
 
 float anumber;
 
@@ -28,22 +28,22 @@ int main (int argc, char* argv[])
 	i=0;
 	while ( fscanf(iFile,"%f",&anumber) != EOF )//car1pos
 	{
-		car1prevpos=anumber;
+		car1pos=anumber;
 		if(fabs(car2pos-car1pos )< 5)//smaller than 5 meters abs distace to car1
 		{	
 			
-			car2speed=car2prevspeed-2; //slow down
+			car2speed=car2prevspeed-10; //slow down
 			if(car2speed<0) //can't have negative speed
 				{car2speed=0;}
 		}
 		else
 		{
-			if (car2prevspeed>=16)//over speed limit, 16cm per 0.1s=1.6m for 1s assume speed limit for road is 60kmh =~1.67m/0.1s
-			{	car2speed=16;}
+			if (car2prevspeed>=60)//over speed limit, 16cm per 0.1s=1.6m for 1s assume speed limit for road is 60kmh =~1.67m/0.1s
+			{	car2speed=60;}
 			else
-			{	car2speed=car2prevspeed+1;}//speed up
+			{	car2speed=car2prevspeed+5;}//speed up
 		}
-		car2pos=car2prevpos+car2speed;
+		car2pos=car2prevpos+(car2speed/3.6);
 		car2prevpos=car2pos;
 		car2prevspeed=car2speed;
 		
