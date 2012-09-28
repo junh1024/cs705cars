2203fcb4399b55bef3926ab3cef90fb007955cff
junh1024@gmail.com <junh1024@gmail.com@36892dda-2c90-5672-c6f2-4098cb2dc41a>
2012-09-28 04:11:27 +0000

    made test respond to lane direction

--- test.c
+++ test.c
@@ -14,18 +14,25 @@ FILE * iFile, * oFile;
 
 int main (int argc, char* argv[])
 {
-	Lane alane;
-	alane.direction=RIGHT;
-	printf("%d",alane.direction );
+	Lane lane1;//a static lane
+	
+	Lane *thelane;
+	
+	thelane=&lane1;//pointer to the current lane
+	
+	thelane->direction=RIGHT;
+	
+	
+	// printf("%d",alane.direction );
 
-	Car firstcar;
+	Car firstcar; //static cars
 	Car secondcar;
 	
 	Car *car1;
 	Car *car2;
 	
-	car1=&firstcar;
-	car2=&secondcar;
+	car1=&firstcar;//pointer to car in front
+	car2=&secondcar;//pointer to car that's following
 	
 	car1->location.x=-5;
 	car2->speed=0;
@@ -35,11 +42,31 @@ int main (int argc, char* argv[])
 	oFile = fopen (argv[2],"w+");
 	
 	i=0;
+	float distancetonextcar;
 	while ( fscanf(iFile,"%f",&anumber) != EOF )//car1pos
 	{
 		car1->location.x=anumber;
+
+		switch(thelane->direction)//compute the distance to the next car based on lane direction & appropriate xy coords
+		{
+			case UP:
+				distancetonextcar=fabs(car2->location.y-car1->location.y );
+				break;
 				
-		if(fabs(car2->location.x-car1->location.x )< 7)//smaller than 7 meters abs distace to car1
+			case DOWN:
+				distancetonextcar=fabs(car2->location.y-car1->location.y );
+				break;
+				
+			case LEFT:
+				distancetonextcar=fabs(car2->location.x-car1->location.x );
+				break;
+				
+			case RIGHT:
+				distancetonextcar=fabs(car2->location.x-car1->location.x );
+				break;
+		}		
+
+		if(distancetonextcar< 7)//smaller than 7 meters abs distace to car1
 		{				
 			car2->speed-=DECEL; //slow down
 		}
@@ -56,7 +83,25 @@ int main (int argc, char* argv[])
 		{
 			car2->speed=60;
 		}
-		car2->location.x+=(car2->speed/3.6);//update position of car2 by adding its speed in m/s
+		
+		switch(thelane->direction)//update position of car2 by adding its speed in m/s
+		{
+			case UP:
+				car2->location.y-=(car2->speed/3.6);
+				break;
+				
+			case DOWN:
+				car2->location.y+=(car2->speed/3.6);
+				break;
+				
+			case LEFT:
+				car2->location.x-=(car2->speed/3.6);
+				break;
+				
+			case RIGHT:
+				car2->location.x+=(car2->speed/3.6);
+				break;
+		}
 
 		fprintf(oFile,"%f\n",car2->location.x);//write pos of car2 to file
 	}
