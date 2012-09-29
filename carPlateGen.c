e8ad4af94b79ea4e8b515d4f6e40e9a6c9033656
json093JS@gmail.com <json093JS@gmail.com@36892dda-2c90-5672-c6f2-4098cb2dc41a>
2012-09-29 03:00:38 +0000

    Generates car plate number

---
+++ carPlateGen.c
@@ -0,0 +1,37 @@
+/** Generates car plate number (3 numbers and 3 chars)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+char* generate() {
+	char letters[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char plate[6];
+	int i;
+	int k;
+	char h;
+	int j;
+	
+	for (i=0;i<3;i++) {
+		k=rand()%10;
+		h=(char)(((int)'0')+k); 
+		plate[i] = h;
+	}
+
+	for (i=0;i<3;i++) {
+		j=rand()%25;
+		plate[i+3] = letters[j];
+	}
+	return plate;
+}
+
+int main() {
+	int i;
+	char* k;
+	srand((unsigned)time(NULL));
+	for(i=0;i<100;i++) {
+		k=generate();
+		printf("%c%c%c%c%c%c\n",k[0],k[1],k[2],k[3],k[4],k[5]);
+	}
+}
